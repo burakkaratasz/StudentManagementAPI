a1317ce4341c78be9e1b4dd9181623e9ff0be124
Burak Karataş <burakkaratasz@outlook.com>
2023-08-04 14:03:29 +0300

    firs commit

---
+++ StudentManagement/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+
+namespace StudentManagement.Models
+{
+    [BsonIgnoreExtraElements]
+    public class Student
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)] // mongoDB to .NET datatype convert
+        public string Id { get; set; } = string.Empty;
+
+        [BsonElement("name")]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement("graduated")]
+        public bool IsGraduated { get; set; }
+
+        [BsonElement("courses")]
+        public string[]? Courses { get; set; }
+
+        [BsonElement("gender")]
+        public string Gender { get; set; } = string.Empty;
+
+        [BsonElement("age")]
+        public int Age { get; set; }
+    }
+}
